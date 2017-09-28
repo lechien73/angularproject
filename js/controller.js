7f44ae415d963761c74025b348575a4f6827eeff
Matt Rudge <matt.rudge@codeinstitute.net>
2017-09-28 07:55:07 +0000

    Register template

--- js/controller.js
+++ js/controller.js
@@ -4,14 +4,8 @@ angular.module("RouteControllers", [])
     })
     .controller("RegisterController", function($scope) {
         
-        $scope.registrationUser = {}
-        
         $scope.submitForm = function() {
-            if ($scope.registrationForm.$valid) {
-                $scope.registrationUser.username = $scope.user.username;
-                $scope.registrationUser.password = $scope.user.password;
-            }
             
-            console.log($scope.registrationUser.username + " " + $scope.registrationUser.password);
+            console.log($scope.user.username + " " + $scope.user.password);
         }
-    })
+    });
